6e4dcc8af1c2c47f1c50318e99d91700f6c865f4
Miguel Arranz <m.arranz.perez@gmail.com>
2022-04-11 15:23:36 +0100

    test

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,8 +1,15 @@
-resource "aws_ecs_cluster" "foo" {
-  name = "white-hart"
+resource "aws_ecs_cluster" "test" {
+  name = "test-circleci-news"
+}
 
-  setting {
-    name  = "containerInsights"
-    value = "enabled"
+resource "aws_ecs_cluster_capacity_providers" "test" {
+  cluster_name = aws_ecs_cluster.test.name
+
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    base              = 1
+    weight            = 100
+    capacity_provider = "FARGATE"
   }
 }
