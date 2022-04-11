c3570b5d93621a9f181122ac1d5491aae5eca504
Miguel Arranz <m.arranz.perez@gmail.com>
2022-04-11 16:31:25 +0100

    test

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -13,3 +13,126 @@ resource "aws_ecs_cluster_capacity_providers" "test" {
     capacity_provider = "FARGATE"
   }
 }
+
+resource "aws_ecs_task_definition" "test" {
+  family                   = "test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 256
+  memory                   = 512
+  container_definitions    = <<TASK_DEFINITION
+[
+  {
+    "name": "nginx",
+    "image": "mijack/circleci-test-image:latest",
+    "cpu": 0,
+    "memory": 512,
+    "essential": true,
+    "portMappings": [
+        {
+          "hostPort": 80,
+          "protocol": "tcp",
+          "containerPort": 80
+        }
+    ]
+  }
+]
+TASK_DEFINITION
+
+  runtime_platform {
+    operating_system_family = "LINUX"
+    cpu_architecture        = "X86_64"
+  }
+}
+
+
+
+
+resource "aws_ecs_service" "test" {
+  name            = "test"
+  cluster         = aws_ecs_cluster.test.id
+  task_definition = aws_ecs_task_definition.test.arn
+  desired_count   = 1
+
+  capacity_provider_strategy { 
+      base              = 1
+      capacity_provider = "FARGATE"
+      weight            = 100 
+    }
+
+  network_configuration {
+    subnets = data.aws_subnets.default.ids
+    security_groups = [ aws_security_group.allow_all.id ]
+    assign_public_ip = true
+  }
+  load_balancer {
+    target_group_arn = aws_lb_target_group.tg.arn
+    container_name   = "nginx"
+    container_port   = 80
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "test"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.default.id
+  target_type = "ip"
+
+}
+resource "aws_lb_listener" "listener80" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_all.id]
+  subnets = data.aws_subnets.default.ids
+  enable_deletion_protection = false
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+resource "aws_security_group" "allow_all" {
+  name        = "allow_all"
+  description = "Allow all inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description      = "traffic from anywhere"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name = "vpc-id"
+    values = [ data.aws_vpc.default.id ]
+  }
+}
