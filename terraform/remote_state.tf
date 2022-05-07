260116149c330825e62badb5255c82f2b260a16e
Miguel Arranz <mijackatmijackdotnet>
2022-05-07 01:26:26 +0100

    change

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -4,7 +4,7 @@
 terraform {
   backend "s3" {
     bucket  = "remote-state-tests-circleci-marranz-news"
-    key     = "${var.environment_name}-infrastructure"
+    key     = "infrastructure"
     region  = "eu-west-1"
     encrypt = true
   }
